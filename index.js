427c75e9830af96179335558a7635db10c315b0b
mdiktushar <md.tushar1804@gmail.com>
2023-05-03 14:44:27 +0600

    server chefs, chef/id

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
-const app = express()
-const port = process.env.PORT || 5000
+var cors = require("cors");
+const app = express();
+const chefs = require("./data/chefs.json");
 
+const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get(`/`, (req, res) => {
-    res.send("Ths server is working. Alhumduillah...!!!")
-})
+  res.send(chefs);
+});
 
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  const chef = chefs.find((chef) => chef.id == id);
+  res.send(chef);
+});
 
 app.listen(port, () => {
-    console.log(`API is running on port: ${port}`);
-})
+  console.log(`API is running on port: ${port}`);
+});
